e14671bb398ea9424482dda9b6f1bde577a54f91
Rasim <rasimmammadoff1@gmail.com>
2022-02-12 16:49:41 +0400

    Roman to int

---
+++ 13_roman_to_int.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        alp = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        ans = 0
+        for i in range(len(s)-1):
+            if alp[s[i]] >= alp[s[i+1]]:
+                ans += alp[s[i]]
+            else:
+                ans -= alp[s[i]]
+        ans = ans + alp[s[-1]]
+        return ans
+            
+
+test = Solution()
+print(test.romanToInt("MCMXCIV"))
