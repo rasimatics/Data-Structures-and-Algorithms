704788476058df1cb77a6319cd5537e8168f84d7
Rasim <rasimmammadoff1@gmail.com>
2022-03-28 12:53:29 +0400

    Stock 2nd way

--- 121_stock.py
+++ 121_stock.py
@@ -13,4 +13,35 @@ class Solution:
             p2 += 1
         
         return profit if profit > 0 else 0
+
+
+    # second way to find also indices
+    prices = [1,4,2]
+    def max_profit(self, prices):
+        buy_price = prices[0]
+        buy_day = 1
+        bought_day = 1
+        sold_day = 1
+        profit = 0
+
+        day = 1
+        for price in prices:
+            if price < buy_price:
+                buy_price = price
+                buy_day = day
+
+            if profit < price - buy_price:
+                profit = price - buy_price
+                bought_day = buy_day
+                sold_day = day
+
+            day += 1
+        
+        print(f"{profit=} {bought_day=} {sold_day=}")
+
+        return profit
+
+
+
+
         
