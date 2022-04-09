987961a2399f057e362988e493fa631c7365a9a8
Rasim <rasimmammadoff1@gmail.com>
2022-04-09 17:52:06 +0400

    Implement Queue using Stacks

--- 232_queue_2_stacks.py
+++ 232_queue_2_stacks.py
@@ -1,92 +1,38 @@
-class Stack:
-    def __init__(self):
-        self.list = []
-        
-    def push(self, val):
-        self.list.append(val)
-        
-    def pop(self):
-        return self.list.pop()
-    
-    def size(self):
-        return len(self.list)
-    
-    def clear(self):
-        self.list.clear()
-    
-    def is_empty(self):
-        return self.size() == 0
-
-    
 class MyQueue:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.stack1 = Stack()
-        self.stack2 = Stack()
-        
+        self.pop_stack = []
+        self.push_stack = []
         
 
     def push(self, x: int) -> None:
-        """
-        Push element x to the back of queue.
-        """
-        self.stack1.push(x)
+        self.push_stack.append(x)
         
 
     def pop(self) -> int:
-        """
-        Removes the element from in front of queue and returns that element.
-        """
-        if not self.empty():
-            for _ in range(self.stack1.size() - 1):
-                self.stack2.push(self.stack1.pop())
+        if self.pop_stack:
+            return self.pop_stack.pop()
+        
+        while self.push_stack:
+            self.pop_stack.append(self.push_stack.pop())
+        
+        return self.pop_stack.pop()
 
-            elem = self.stack1.pop()
-
-            self.stack1.list = list(self.stack2.list[::-1])
-            self.stack2.clear()
-
-            return elem
-        return -1
-            
-
+    
     def peek(self) -> int:
-        """
-        Get the front element.
-        """
-        if not self.empty():
-            for _ in range(self.stack1.size() - 1):
-                self.stack2.push(self.stack1.pop())
+        if self.pop_stack:
+            return self.pop_stack[-1]
+        return self.push_stack[0]
 
-            elem = self.stack1.pop()
-
-            self.stack1.list = list([elem] + self.stack2.list[::-1])
-            self.stack2.clear()
-
-            return elem
-        return -1
         
-
     def empty(self) -> bool:
-        """
-        Returns whether the queue is empty.
-        """
-        return self.stack1.is_empty()
+        return not(self.push_stack or self.pop_stack)
         
 
-myQueue = MyQueue() 
-myQueue.push(1)
-myQueue.push(2)
-myQueue.push(3)
-myQueue.push(4)
-print(myQueue.stack1.list)
-print(myQueue.pop())
-myQueue.push(5)
-print(myQueue.stack1.list)
-print(myQueue.pop())
-print(myQueue.pop())
-print(myQueue.pop())
-print(myQueue.pop())
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
