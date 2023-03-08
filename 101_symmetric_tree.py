66c296f23dbf732bdf8e4aeb28157d08a8db1939
Rasim Mammadov <47504114+rasimatics@users.noreply.github.com>
2023-03-08 15:49:39 +0400

    Symmetric Tree

---
+++ 101_symmetric_tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:    
+        def checkSymmetric(root1, root2):
+            if not root1 and not root2:
+                return True
+            
+            if not root1 or not root2:
+                return False
+
+            if root1.val == root2.val:
+                return checkSymmetric(root1.left, root2.right) and checkSymmetric(root1.right, root2.left)
+            
+            return False
+
+        return checkSymmetric(root, root)
